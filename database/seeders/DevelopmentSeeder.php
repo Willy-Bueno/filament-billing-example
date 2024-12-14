c80dc53f7f35df12ba57c8e7c6de538e7ab88012
Willy-Bueno <willybueno090@gmail.com>
2024-12-13 21:29:59 -0300

    feat: add development seeder

---
+++ database/seeders/DevelopmentSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DevelopmentSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => '$2y$12$rXHn4Vchoy91KuZdFB6VG.kNxYiyClG/Dh2oOQCQC/h8WZHKdrVl6',
+            'remember_token' => '5NTlKi8O3tWSAt1W7Q5gIiXSLsQvJ1ebcPRASGG9u98jVyV7am1Kh3FBQHNT',
+        ]);
+    }
+}
